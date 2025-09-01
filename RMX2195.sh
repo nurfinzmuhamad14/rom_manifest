37f70ae4a792295c0efd0ec7cd97775a27aea3d0
UdyneOS <51253393+nurfinzmuhamad14@users.noreply.github.com>
2025-09-02 06:21:00 +0700

    Update RMX2195.sh

--- RMX2195.sh
+++ RMX2195.sh
@@ -10,6 +10,7 @@ rm -rf kernel/realme/RMX2195
 rm -rf vendor/realme/RMX2195
 rm -rf device/realme/bengal-common
 rm -rf hardware/oplus
+rm -rf hardware/qcom-caf/sm8250
 
 # Device Tree
 git clone https://github.com/SM4250-Dev/device_realme_RMX2195.git -b los device/realme/RMX2195; \
@@ -24,6 +25,20 @@ git clone https://github.com/SM4250-Dev/kernel_realme_RMX2195.git -b stable --de
 
 # Hardware oplus
 git clone https://github.com/LineageOS/android_hardware_oplus.git hardware/oplus -b lineage-19.1; \
+
+# Remote
+SM8250="https://github.com/custom-crdroid/hardware-qcom-caf-sm8250-xxx.git"
+# CLO HALs
+git clone $SM8250 -b audio hardware/qcom-caf/sm8250/audio
+git clone $SM8250 -b media hardware/qcom-caf/sm8250/display
+git clone $SM8250 -b display hardware/qcom-CLO/sm6115/media
+
+# Vendor common
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_wfd-commonsys -b lineage-18.1 vendor/qcom/common/system/wfd
+git clone https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/location -b LA.UM.9.15.1.r1-04800-KAMORTA.0 vendor/qcom/common/system/gps
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_dataservices -b lineage-18.1 vendor/qcom/opensource/dataservices
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_display-commonsys -b lineage-19.0 vendor/qcom/opensource/display
+
 
 . build/envsetup.sh; \
 
